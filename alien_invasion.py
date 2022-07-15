b1a0bcbee3dc679047534342ac463bb27f7d87f6
Bakashev Sergey <BakashevSergey@gmail.com>
2022-07-15 17:36:19 +0300

    Game initilisation and create object game

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,16 +5,19 @@ def run_game():
     """Game initilisation and create object game"""
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
+    bg_color=(230,230,230)
     pygame.display.set_caption('Alien Invasion')
 
-    """Basic circle run"""
+    #Basic circle run
 
     while True:
+        #Tracking Keyboard and Mouse Events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.BUTTON_LEFT:
                 pass
+        screen.fill(bg_color)
         pygame.display.flip()
 
 run_game()
